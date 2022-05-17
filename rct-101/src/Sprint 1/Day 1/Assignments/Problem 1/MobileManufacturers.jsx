38fa5363fe2f159af0c72c6be81c689dcfb1988e
Syed25794 <syedgulmohammad25794@gmail.com>
2022-05-17 21:55:20 +0530

    Problem 1 done

---
+++ rct-101/src/Sprint 1/Day 1/Assignments/Problem 1/MobileManufacturers.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const MobileManufacturers=()=>{
+    return(
+        <>
+        <h1>Mobile Manufacturers</h1>
+        <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Mircomax</li>
+        <li style={{"listStyleType":"circle"}}>Apple</li>
+        </ul>
+        </>
+    )
+}
+
+export default MobileManufacturers;
---
+++ rct-101/src/Sprint 1/Day 1/Assignments/Problem 1/MobileOS.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function MobileOS(){
+    return(
+        <>
+        <h1>Mobile Operating System</h1>
+        <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Window Phone</li>
+        </ul>
+        </>
+    )
+}
+
+export default MobileOS;
